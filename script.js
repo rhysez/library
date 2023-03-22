c6a769a345019c57da5c76c91250a75473be70de
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-03-22 21:59:21 +0000

    add function addBookToLibrary()

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const buttonNewBook = document.querySelector('#newBook');
 const buttonUpdate = document.querySelector('#update');
 const form = document.querySelector('form');
 
-let bookName = document.querySelector('#bookname');
+let bookTitle = document.querySelector('#booktitle');
 let bookAuthor = document.querySelector('#bookauthor')
 let bookPages = document.querySelector('#bookpages');
 
@@ -19,20 +19,21 @@ buttonNewBook.addEventListener('click', () => {
 // takes input data from form and places into myLibrary[]
 buttonAddBook.addEventListener('click', () => {
     grid.appendChild(document.createElement('div')).className = 'book';
-    grid.lastChild.textContent = bookName.value;
-    bookStorage.push(bookName.value);
+    grid.lastChild.textContent = bookTitle.value;
+    bookStorage.push(bookTitle.value);
     addBookToLibrary();
 })
 
 // object constructor for books
-function Book(name, author, pages) {
-    this.name = name
+function Book(title, author, pages) {
+    this.title = title
     this.author = author
     this.pages = pages
 }
 
 function addBookToLibrary() {
-    
+    let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
+    myLibrary.push(newBook);
 }
 
 function displayBooks() {
